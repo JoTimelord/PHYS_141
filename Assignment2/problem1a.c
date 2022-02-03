c4e1ec3a2bb781bcc45cdbcef452ff41cc3bfcc6
Yuntong Zhou <yuz102@its-may4623-15.ucsd.edu>
2022-02-03 14:50:52 -0800

    deleted:    Assignment2/Jupiter.dat 	new file:   Assignment2/problem1a.c

---
+++ Assignment2/problem1a.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// global variables
+int M=
+int G=1;
+
+
+
+
+int main(argc, argv)
+int argc;
+char *argv[];
+
+
